44e96ae9324aa4de2a16b7092850174788e65b12
GonzaloKraus <yo-gonza@hotmail.es>
2023-04-16 19:19:40 -0300

    Se agrega el proyecto

---
+++ SumaDeHorasMinutosSegundos/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SumaDeHorasMinutosSegundos
+{
+    partial class FSumaHorasMinutosSegundos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.tHoras1 = new System.Windows.Forms.TextBox();
+            this.tMinutos1 = new System.Windows.Forms.TextBox();
+            this.tSegundos1 = new System.Windows.Forms.TextBox();
+            this.tHoras2 = new System.Windows.Forms.TextBox();
+            this.tMinutos2 = new System.Windows.Forms.TextBox();
+            this.tSegundos2 = new System.Windows.Forms.TextBox();
+            this.lHoras2 = new System.Windows.Forms.Label();
+            this.lMinutos2 = new System.Windows.Forms.Label();
+            this.lSegundos2 = new System.Windows.Forms.Label();
+            this.lHoras1 = new System.Windows.Forms.Label();
+            this.lMinutos1 = new System.Windows.Forms.Label();
+            this.lSegundos1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.Location = new System.Drawing.Point(459, 256);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(71, 35);
+            this.bCerrar.TabIndex = 0;
+            this.bCerrar.Text = "Cerrar";
+            this.bCerrar.UseVisualStyleBackColor = true;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.Location = new System.Drawing.Point(204, 185);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(71, 48);
+            this.bCalcular.TabIndex = 1;
+            this.bCalcular.Text = "Calcular";
+            this.bCalcular.UseVisualStyleBackColor = true;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // tHoras1
+            // 
+            this.tHoras1.Location = new System.Drawing.Point(120, 70);
+            this.tHoras1.Name = "tHoras1";
+            this.tHoras1.Size = new System.Drawing.Size(76, 20);
+            this.tHoras1.TabIndex = 2;
+            // 
+            // tMinutos1
+            // 
+            this.tMinutos1.Location = new System.Drawing.Point(120, 96);
+            this.tMinutos1.Name = "tMinutos1";
+            this.tMinutos1.Size = new System.Drawing.Size(76, 20);
+            this.tMinutos1.TabIndex = 3;
+            // 
+            // tSegundos1
+            // 
+            this.tSegundos1.Location = new System.Drawing.Point(120, 122);
+            this.tSegundos1.Name = "tSegundos1";
+            this.tSegundos1.Size = new System.Drawing.Size(76, 20);
+            this.tSegundos1.TabIndex = 4;
+            // 
+            // tHoras2
+            // 
+            this.tHoras2.Location = new System.Drawing.Point(308, 70);
+            this.tHoras2.Name = "tHoras2";
+            this.tHoras2.Size = new System.Drawing.Size(76, 20);
+            this.tHoras2.TabIndex = 5;
+            // 
+            // tMinutos2
+            // 
+            this.tMinutos2.Location = new System.Drawing.Point(308, 96);
+            this.tMinutos2.Name = "tMinutos2";
+            this.tMinutos2.Size = new System.Drawing.Size(76, 20);
+            this.tMinutos2.TabIndex = 6;
+            // 
+            // tSegundos2
+            // 
+            this.tSegundos2.Location = new System.Drawing.Point(308, 122);
+            this.tSegundos2.Name = "tSegundos2";
+            this.tSegundos2.Size = new System.Drawing.Size(76, 20);
+            this.tSegundos2.TabIndex = 7;
+            // 
+            // lHoras2
+            // 
+            this.lHoras2.AutoSize = true;
+            this.lHoras2.Location = new System.Drawing.Point(244, 73);
+            this.lHoras2.Name = "lHoras2";
+            this.lHoras2.Size = new System.Drawing.Size(38, 13);
+            this.lHoras2.TabIndex = 8;
+            this.lHoras2.Text = "Horas:";
+            // 
+            // lMinutos2
+            // 
+            this.lMinutos2.AutoSize = true;
+            this.lMinutos2.Location = new System.Drawing.Point(244, 103);
+            this.lMinutos2.Name = "lMinutos2";
+            this.lMinutos2.Size = new System.Drawing.Size(47, 13);
+            this.lMinutos2.TabIndex = 9;
+            this.lMinutos2.Text = "Minutos:";
+            // 
+            // lSegundos2
+            // 
+            this.lSegundos2.AutoSize = true;
+            this.lSegundos2.Location = new System.Drawing.Point(244, 129);
+            this.lSegundos2.Name = "lSegundos2";
+            this.lSegundos2.Size = new System.Drawing.Size(58, 13);
+            this.lSegundos2.TabIndex = 10;
+            this.lSegundos2.Text = "Segundos:";
+            // 
+            // lHoras1
+            // 
+            this.lHoras1.AutoSize = true;
+            this.lHoras1.Location = new System.Drawing.Point(62, 73);
+            this.lHoras1.Name = "lHoras1";
+            this.lHoras1.Size = new System.Drawing.Size(38, 13);
+            this.lHoras1.TabIndex = 11;
+            this.lHoras1.Text = "Horas:";
+            // 
+            // lMinutos1
+            // 
+            this.lMinutos1.AutoSize = true;
+            this.lMinutos1.Location = new System.Drawing.Point(62, 99);
+            this.lMinutos1.Name = "lMinutos1";
+            this.lMinutos1.Size = new System.Drawing.Size(47, 13);
+            this.lMinutos1.TabIndex = 12;
+            this.lMinutos1.Text = "Minutos:";
+            // 
+            // lSegundos1
+            // 
+            this.lSegundos1.AutoSize = true;
+            this.lSegundos1.Location = new System.Drawing.Point(60, 125);
+            this.lSegundos1.Name = "lSegundos1";
+            this.lSegundos1.Size = new System.Drawing.Size(58, 13);
+            this.lSegundos1.TabIndex = 13;
+            this.lSegundos1.Text = "Segundos:";
+            // 
+            // FSumaHorasMinutosSegundos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 303);
+            this.Controls.Add(this.lSegundos1);
+            this.Controls.Add(this.lMinutos1);
+            this.Controls.Add(this.lHoras1);
+            this.Controls.Add(this.lSegundos2);
+            this.Controls.Add(this.lMinutos2);
+            this.Controls.Add(this.lHoras2);
+            this.Controls.Add(this.tSegundos2);
+            this.Controls.Add(this.tMinutos2);
+            this.Controls.Add(this.tHoras2);
+            this.Controls.Add(this.tSegundos1);
+            this.Controls.Add(this.tMinutos1);
+            this.Controls.Add(this.tHoras1);
+            this.Controls.Add(this.bCalcular);
+            this.Controls.Add(this.bCerrar);
+            this.Name = "FSumaHorasMinutosSegundos";
+            this.Text = "Suma de Horas,Minutos y Segundos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCerrar;
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.TextBox tHoras1;
+        private System.Windows.Forms.TextBox tMinutos1;
+        private System.Windows.Forms.TextBox tSegundos1;
+        private System.Windows.Forms.TextBox tHoras2;
+        private System.Windows.Forms.TextBox tMinutos2;
+        private System.Windows.Forms.TextBox tSegundos2;
+        private System.Windows.Forms.Label lHoras2;
+        private System.Windows.Forms.Label lMinutos2;
+        private System.Windows.Forms.Label lSegundos2;
+        private System.Windows.Forms.Label lHoras1;
+        private System.Windows.Forms.Label lMinutos1;
+        private System.Windows.Forms.Label lSegundos1;
+    }
+}
+
---
+++ SumaDeHorasMinutosSegundos/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumaDeHorasMinutosSegundos
+{
+    public partial class FSumaHorasMinutosSegundos : Form
+    {
+        public FSumaHorasMinutosSegundos()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bCalcular_Click(object sender, EventArgs e)
+        {
+            int horas1 = Convert.ToInt32(tHoras1.Text);
+            int minutos1 = Convert.ToInt32(tMinutos1.Text);
+            int segundos1 = Convert.ToInt32(tSegundos1.Text);
+
+            int horas2 = Convert.ToInt32(tHoras2.Text);
+            int minutos2 = Convert.ToInt32(tMinutos2.Text);
+            int segundos2 = Convert.ToInt32(tSegundos2.Text);
+
+
+            int totalSegundos = (horas1 + horas2) * 3600 + (minutos1 + minutos2) * 60 + (segundos1 + segundos2);
+
+
+            int horasTotales = totalSegundos / 3600;
+            int minutosTotales = (totalSegundos % 3600) / 60;
+            int segundosTotales = totalSegundos % 60;
+
+            MessageBox.Show("El resultado son " + horasTotales + " Horas " + minutosTotales + " Minutos " + segundosTotales + " Segundos");
+
+        }
+    }
+} 
